2a320bbd4db7230057a85f23ea5dc38de003eb91
samithiwat <samithiwat2544@gmail.com>
2023-04-27 16:39:16 +0700

    feat: add documentation comments

--- dto.go
+++ dto.go
@@ -1,11 +1,15 @@
 package repositorysdk
 
+// QueryResult is a struct that holds the result of an Opensearch query, including the time taken to execute the query,
+// whether the query timed out, and information about the shards.
 type QueryResult struct {
 	Took    uint  `json:"took"`
 	Timeout bool  `json:"timeout"`
 	Shards  Shard `json:"_shards"`
 }
 
+// Shard is a struct that holds information about the shards in an Opensearch cluster, including the total number of shards,
+// the number of successful shards, the number of skipped shards, and the number of failed shards.
 type Shard struct {
 	Total      uint `json:"total"`
 	Successful uint `json:"successful"`
--- entity.go
+++ entity.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+// Base is a struct that holds common fields for database tables, including the ID, creation and update timestamps,
+// and soft deletion timestamp.
 type Base struct {
 	ID        *uuid.UUID     `json:"id" gorm:"primary_key"`
 	CreatedAt time.Time      `json:"created_at" gorm:"type:timestamp;autoCreateTime:nano"`
@@ -14,6 +16,7 @@ type Base struct {
 	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index;type:timestamp"`
 }
 
+// BeforeCreate is a GORM callback that generates a new UUID for the ID field before creating a new record in the database.
 func (b *Base) BeforeCreate(_ *gorm.DB) error {
 	if b.ID == nil {
 		b.ID = gosdk.UUIDAdr(uuid.New())
@@ -22,12 +25,15 @@ func (b *Base) BeforeCreate(_ *gorm.DB) error {
 	return nil
 }
 
+// BaseHardDelete is a struct that holds common fields for database tables, including the ID and creation and update timestamps,
+// but excludes soft deletion timestamp.
 type BaseHardDelete struct {
 	ID        *uuid.UUID `json:"id" gorm:"primary_key"`
 	CreatedAt time.Time  `json:"created_at" gorm:"type:timestamp;autoCreateTime:nano"`
 	UpdatedAt time.Time  `json:"updated_at" gorm:"type:timestamp;autoUpdateTime:nano"`
 }
 
+// BeforeCreate is a GORM callback that generates a new UUID for the ID field before creating a new record in the database.
 func (b *BaseHardDelete) BeforeCreate(_ *gorm.DB) error {
 	if b.ID == nil {
 		b.ID = gosdk.UUIDAdr(uuid.New())
@@ -36,6 +42,8 @@ func (b *BaseHardDelete) BeforeCreate(_ *gorm.DB) error {
 	return nil
 }
 
+// PaginationMetadata is a struct that holds pagination metadata including the number of items per page, the current page,
+// the total number of items, and the total number of pages.
 type PaginationMetadata struct {
 	ItemsPerPage int
 	ItemCount    int
@@ -44,10 +52,12 @@ type PaginationMetadata struct {
 	TotalPage    int
 }
 
+// GetOffset is a method that calculates the offset for the current page based on the number of items per page.
 func (p *PaginationMetadata) GetOffset() int {
 	return (p.GetCurrentPage() - 1) * p.GetItemPerPage()
 }
 
+// GetItemPerPage is a method that returns the number of items per page, ensuring that the value is within a certain range.
 func (p *PaginationMetadata) GetItemPerPage() int {
 	if p.ItemsPerPage < 10 {
 		p.ItemsPerPage = 10
@@ -59,6 +69,7 @@ func (p *PaginationMetadata) GetItemPerPage() int {
 	return p.ItemsPerPage
 }
 
+// GetCurrentPage is a method that returns the current page, ensuring that the value is at least 1.
 func (p *PaginationMetadata) GetCurrentPage() int {
 	if p.CurrentPage < 1 {
 		p.CurrentPage = 1
@@ -66,6 +77,8 @@ func (p *PaginationMetadata) GetCurrentPage() int {
 	return p.CurrentPage
 }
 
+// CalItemPerPage is a method that calculates the number of items per page based on the total number of items and the
+// desired number of items per page.
 func (p *PaginationMetadata) CalItemPerPage() {
 	if p.ItemCount < p.ItemsPerPage {
 		p.ItemsPerPage = p.ItemCount
